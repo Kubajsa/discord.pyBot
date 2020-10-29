81888bc49210a7d87bcb172b24cae50bc343c11f
Kubajsa <unconfigured@null.spigotmc.org>
2020-10-29 16:14:06 +0100

    Trade command + Help command

--- bot.py
+++ bot.py
@@ -29,11 +29,23 @@ async def on_message(message):
 async def about(ctx):
     embed = discord.Embed(title="Kubajsa's Bot", description="Here is some info about this bot:", color=discord.Color.dark_green())
     embed.add_field(name="Author", value="Coded by Kubajsa in Python with <3", inline= True)
-    embed.add_field(name="Current commands", value=".about .hello .ping .whois .trade [kit you have] [kit you want]", inline=True)
+    embed.add_field(name="Current commands", value=".help .about .hello .ping .whois .trade [kit you have] [kit you want]", inline=True)
     embed.add_field(name="Current events", value="Automatically adds yes and no reactions in polls and suggestions. Deletes messages without images in 6b6t-screentshots", inline=False)
     embed.set_thumbnail(url= client.user.avatar_url)
     embed.set_footer(icon_url= ctx.author.avatar_url, text=f"Requested by {ctx.author.name}")
     await ctx.send(embed=embed)
+
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(title="Commands", description="Commands and usages:", color=discord.Color.blue())
+    embed.add_field(".about", value="Shows you some info about the bot", inline=False)
+    embed.add_field(".help", value="Shows this message", inline=False)
+    embed.add_field(".hello", value="Says hello to you", inline=False)
+    embed.add_field(".ping", value="Checks the bot's ping", inline=False)
+    embed.add_field(".whois (@Kubajsa)", value="Shows you some info about the user you mention", inline=False)
+    embed.add_field(".about Kit_you_want Kit_you_have", value="Makes a trade message", inline=False)
+    embed.set_footer(icon_url= ctx.author.avatar_url, text=f"Requested by {ctx.author.name}")
 
 
 @client.command(aliases=['user'])
@@ -57,11 +69,11 @@ async def ping(ctx):
 
 
 @client.command()
-async def trade(ctx, kithave, kitwant):
+async def trade(ctx, kitwant, kithave):
     await ctx.channel.purge(limit=1)
     embed = discord.Embed(title="New trade!", description=f"Trade by: {ctx.author.mention}", color=discord.Color.orange())
     embed.set_thumbnail(url=ctx.author.avatar_url)
-    embed.add_field(name="Trade:", value=f"{ctx.author.mention} wants {kithave} for {kitwant}")
+    embed.add_field(name="Trade:", value=f"{ctx.author.mention} wants {kitwant} for {kithave}")
     await ctx.send(embed=embed)
 
 
