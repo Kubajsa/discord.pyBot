428443d9184ea48455bdf4750d046ed3328dfdfa
Kubajsa <unconfigured@null.spigotmc.org>
2020-11-03 17:28:39 +0100

    Updated .about
    Now shows "Use .commands to view all available commands" instead of the actual commands

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ async def on_message(message):
 async def about(ctx):
     embed = discord.Embed(title="Kubajsa's Bot", description="Here is some info about this bot:", color=discord.Color.dark_green())
     embed.add_field(name="About", value="Coded by Kubajsa in Python with <3. The GitHub repo for the bot is https://github.com/Kubajsa/discord.pyBot", inline= False)
-    embed.add_field(name="Current commands", value=".commands .about .hello .ping .whois .trade [kit you want] [kit you have]", inline=False)
+    embed.add_field(name="Current commands", value="Use .commands to view all available commands", inline=False)
     embed.add_field(name="Current events", value="Automatically adds yes and no reactions in polls and suggestions. Deletes messages without images in 6b6t-screentshots. Events so far work only in http://discord.gg/m33xSJX", inline=False)
     embed.set_thumbnail(url= client.user.avatar_url)
     embed.set_footer(icon_url= ctx.author.avatar_url, text=f"Requested by {ctx.author.name}")
